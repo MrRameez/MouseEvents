874b8afb369af28c6795aee1eb835a8de56e406b
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2024-06-18 23:33:56 +0500

    Change Image

---
+++ app.js
@@ -0,0 +1,19 @@
+const newImage = document.querySelector("#image1")
+const newImage2 = document.querySelector("#image2")
+
+
+// first card //
+function setNewImage() {
+    newImage.src = "./imag/images.jpeg";
+}
+function setOldImage() {
+    newImage.src = "./imag/download.jpeg"
+}
+
+// second card //
+function setImage() {
+    newImage2.src = "./imag/download (2).jpeg";
+}
+function setOutImage() {
+    newImage2.src = "./imag/download 1.jpeg"
+}
